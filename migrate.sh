ccec80f6d42d91a2d07ef83dbd1941f78bcb4e46
culiiik <mr.pavel.novak@gmail.com>
2024-08-09 01:43:48 +0200

    update

--- migrate.sh
+++ migrate.sh
@@ -5,3 +5,10 @@ set -e
 
 export PGPASSWORD=${pgdb_password}
 
+INIT_DONE=$(psql -h pgdb -U pgdb pgdb -t -c "SELECT 1 FROM pg_database WHERE datname = 'zabbix';"|tr -d '[:blank:]')
+
+if [[ ${INIT_DONE} -ne 1 ]]; then
+        echo "Zabbic DB init"
+        psql -h pgdb -U pgdb pgdb -t -c "CREATE DATABASE zabbix;"
+        sudo zcat /usr/share/zabbix-sql-scripts/postgresql/server.sql.gz | sudo psql -h pgdb -U pgdb -d zabbix
+fi
